e452915bb39a64bc89f3f5578e2462ad56d289e7
MacMillan Matlaila <83666081+MacmillanMatlaila@users.noreply.github.com>
2022-07-18 09:40:52 +0200

    My first few javascript lines

---
+++ index.js
@@ -0,0 +1,15 @@
+//document.getElementById("count-el").innerText = 5;
+
+let firstBatch = 5
+let secondBatch = 7
+
+let count = firstBatch + secondBatch
+
+console.log(count)
+
+let myAge = 26
+let humanDogRatio = myAge * 7
+
+let count = humanDogRatio
+
+console.log(count)
